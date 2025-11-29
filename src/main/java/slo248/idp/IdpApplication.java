452369d5b8b5aafc36e5d3b99a71db54cb4c5f69
Phuc Nguyen Hoang <nhplhp2004@gmail.com>
2025-11-29 15:06:33 +0000

    Init Spring

---
+++ src/main/java/slo248/idp/IdpApplication.java
@@ -0,0 +1,13 @@
+package slo248.idp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/slo248/idp/IdpApplicationTests.java
@@ -0,0 +1,13 @@
+package slo248.idp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
